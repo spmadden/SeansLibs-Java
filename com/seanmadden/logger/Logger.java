47c7ed25ef81de1357bef1760167e0a2beed7ce8
Sean <sean@seanmadden.net>
2009-12-10 19:16:13 -0500

    Finished a basic logger for debugging purposes.

--- com/seanmadden/logger/Logger.java
+++ com/seanmadden/logger/Logger.java
@@ -3,7 +3,9 @@
  */
 package com.seanmadden.logger;
 
-import java.util.Hashtable;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.io.*;
 
 /**
  * @author spmfa
@@ -12,28 +14,78 @@ import java.util.Hashtable;
 public class Logger {
 
 	private static Hashtable<Object, Logger> loggers = new Hashtable<Object, Logger>();
+	private Vector<Writer> outStreams = new Vector<Writer>();
+	private static long startMilli = System.currentTimeMillis();
+	private static DecimalFormat formatter = new DecimalFormat("00000000");
+	private Object key = null;
 	
 	/**
 	 * 
 	 */
-	private Logger() {
-		// TODO Auto-generated constructor stub
+	private Logger(Object key) {
+		this.key = key;
+		addOutputStream(System.out);
 	}
 	
-	public static Logger getLogger(Object toLog){
+	private Logger(Object key, OutputStream out){
+		this.key = key;
+		addOutputStream(out);
+	}
+	
+	public static synchronized Logger getLogger(Object toLog){
 		if(loggers.get(toLog) == null){
-			Logger log = new Logger();
+			Logger log = new Logger(toLog);
 			loggers.put(toLog, log);
 		}
 		return loggers.get(toLog);
 	}
 	
-	public void debug(LogEvent ev, String message){
+	public static synchronized Logger getLogger(Object toLog, OutputStream out){
+		if(loggers.contains(toLog)){
+			Logger log = loggers.get(toLog);
+			log.addOutputStream(out);
+			return log;
+		}else{
+			Logger log = new Logger(toLog, out);
+			loggers.put(toLog, log);
+			return log;
+		}
+	}
+	
+	public void debug(LogEvent ev, String message) {
+		long curMilli = System.currentTimeMillis() - startMilli;
+		String toWrite = "["+formatter.format(curMilli)+"] " + key+" [" + ev.toString() + "] " + message + "\n";
+		for(Writer out : outStreams){
+			synchronized(Logger.class){
+				try {
+					out.write(toWrite);
+					out.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public synchronized void setOptions(){
 		
 	}
 	
-	public void setOptions(){
-		
+	public synchronized void addWriter(Writer writer){
+		outStreams.add(writer);
 	}
 	
+	public synchronized void addOutputStream(OutputStream out){
+		outStreams.add(new OutputStreamWriter(out));
+	}
+	public static void main(String[] args){
+		Logger log = Logger.getLogger(Logger.class);
+		log.debug(LogEvent.DEBUG, "This is a debug message");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		log.debug(LogEvent.FATAL, "FATAL ERROR");
+	}
 }
