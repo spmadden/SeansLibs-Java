1353966eaa80509333bce8630265d30d7fe3f4a5
Sean Madden <sean@seanmadden.net>
2011-06-30 15:59:19 -0400

    Updating the layouter

--- com/seanmadden/graphing/layouts/SpringLayouter.java
+++ com/seanmadden/graphing/layouts/SpringLayouter.java
@@ -1,21 +1,21 @@
 /*
  * SpringLayouter.java
- * 
+ *
  * Copyright (C) 2008 Sean P Madden
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * If you would like to license this code under the GNU LGPL, please see
  * http://www.seanmadden.net/licensing for details.
  */
@@ -27,141 +27,130 @@ import com.seanmadden.graphing.struct.*;
 
 public class SpringLayouter implements Layout, GraphListener {
 
-        private Hashtable<Node, Double> xForces = new Hashtable<Node, Double>();
+	private Hashtable<Node, Double> xForces = new Hashtable<Node, Double>();
 
-        private Hashtable<Node, Double> yForces = new Hashtable<Node, Double>();
+	private Hashtable<Node, Double> yForces = new Hashtable<Node, Double>();
 
-        private double idealEdgeLength = 100;
+	private double idealEdgeLength = 100;
 
-        private double maxMove = 10;
+	private double maxMove = 10;
 
-        private boolean graphChanged = true;
+	private boolean graphChanged = true;
 
-        private Graph lastLayout = null;
+	private Graph lastLayout = null;
 
-        public void doLayout(Graph g) {
-                // if(!graphChanged && g == lastLayout) return;
-                // lastLayout = g;
+	/*
+	 * k = sqrt(area / #nodes) * .6 * 1 / canvas scale.
+	 */
+	private double k = .5;
 
-                Collection<Node> nodes = g.getAllNodes();
+	public void doLayout(Graph g) {
+		// if(!graphChanged && g == lastLayout) return;
+		// lastLayout = g;
+		double temp = 20;
 
-                HashSet<Node> hit = new HashSet<Node>();
+		Collection<Node> nodes = g.getAllNodes();
 
-                for (Node src : nodes) {
-                        hit.add(src);
-                        for (Node dst : nodes) {
-                                if (hit.contains(dst)) continue;
-//                                repulsiveForce(src, dst);
-                                // System.out.println(src + "=>" + dst);
-                                // System.out.print("x: " + xForces.get(dst));
-                                // System.out.println(" y: " +
-                                // yForces.get(dst));
-                        }
-                }
+		HashSet<Node> hit = new HashSet<Node>();
 
-                for (Edge ed : g.getAllEdges()) {
-                        attractiveForce(ed.getSource(), ed.getDestination());
-                }
-                double fx, fy;
-                for (Node nd : g.getAllNodes()) {
-                        fx = xForces.get(nd);
-                        fy = yForces.get(nd);
-                        nd.getPoint().x += Math.abs(fx)
-                                        / fx
-                                        * ((Math.abs(fx) > maxMove) ? maxMove
-                                                        : fx);
-                        nd.getPoint().y += Math.abs(fy)
-                                        / fy
-                                        * ((Math.abs(fy) > maxMove) ? maxMove
-                                                        : fy);
+		for (Node src : nodes) {
+			hit.add(src);
+			for (Node dst : nodes) {
+				if (hit.contains(dst))
+					continue;
+				repulsiveForce(src, dst);
+			}
+		}
 
-                        // System.out.println(nd.getPoint());
-                }
+		for (Edge ed : g.getAllEdges()) {
+			attractiveForce(ed.getSource(), ed.getDestination());
+		}
+		double fx, fy;
+		for (Node nd : g.getAllNodes()) {
+			fx = Math.min(temp, Math.max(xForces.get(nd), -temp));
+			fy = Math.min(temp, Math.max(yForces.get(nd), -temp));
+			
+			fx += nd.getPoint().x;
+			fy += nd.getPoint().y;
+			nd.getPoint().setLocation(fx, fy);
 
-                xForces.clear();
-                yForces.clear();
-                System.out.println("emptied.");
+			// System.out.println(nd.getPoint());
+		}
+		temp *= .995;
+		xForces.clear();
+		yForces.clear();
+		System.out.println("emptied.");
 
-        }
+	}
 
-        public boolean isLayoutable(Graph g) {
-                return false;
-        }
+	public boolean isLayoutable(Graph g) {
+		return false;
+	}
 
-        private double distance2(Node a, Node b) {
-                return (b.getPoint().x - a.getPoint().x)
-                                + +(b.getPoint().y - a.getPoint().y);
-        }
+	private double distance2(Node a, Node b) {
+		return (b.getPoint().x - a.getPoint().x)
+				+ +(b.getPoint().y - a.getPoint().y);
+	}
 
-        private double distance3(Node a, Node b) {
-                return distance2(a, b) * distance(a, b);
-        }
+	private double distance3(Node a, Node b) {
+		return distance2(a, b) * distance(a, b);
+	}
 
-        private double distance(Node a, Node b) {
-                return Math.sqrt(distance2(a, b));
-        }
+	private double distance(Node a, Node b) {
+		return Math.sqrt(distance2(a, b));
+	}
 
-        private void repulsiveForce(Node a, Node b) {
-                double dx = a.getPoint().x - b.getPoint().x;
-                double dy = a.getPoint().y - b.getPoint().y;
-                dx /= distance2(a, b);
-                dy /= distance2(a, b);
+	private void attractiveForce(Node a, Node b) {
+		double dx = b.getPoint().x - a.getPoint().x;
+		double dy = b.getPoint().y - a.getPoint().y;
+		double x = Math.sqrt(dx * dx + dy * dy);
 
-                System.out.println("dx: " + dx + " dy:" + dy);
+		x = (x - k) / x;
 
-                xForceAdd(a, -dx);
-                yForceAdd(a, -dy);
-                xForceAdd(b, .000001);
-                yForceAdd(b, .000001);
+		xForceAdd(a, dx);
+		yForceAdd(a, dy);
+		xForceAdd(b, -dx);
+		xForceAdd(b, -dy);
 
-        }
+	}
 
-        private void attractiveForce(Node a, Node b) {
-                double dx = a.getPoint().x - b.getPoint().x;
-                double dy = a.getPoint().y - b.getPoint().y;
-                System.out.println();
-                double distance = distance(a,b);
-                if(distance == 0 || Double.isNaN(distance)){
-                	distance = .001;
-                }
-                final double ratio = (distance - idealEdgeLength)
-                                / distance;
+	private void repulsiveForce(Node a, Node b) {
+		double dx = b.getPoint().x - a.getPoint().x;
+		double dy = b.getPoint().y - a.getPoint().y;
 
-                dx *= 2 * ratio;
-                dy *= 2 * ratio;
+		double x = 2 * (k) / ((dx * dx) + (dy * dy));
 
-                 System.out.println("dx: " + -dx + " dy: " + -dy);
-                 System.out.println(ratio);
-                //                
-                xForceAdd(a, dx);
-                yForceAdd(a, dy);
-                xForceAdd(b, .00000001);
-                yForceAdd(b, .00000001);
-        }
+		dx *= x;
+		dy *= x;
+		xForceAdd(a, -dx);
+		yForceAdd(a, -dy);
+		xForceAdd(b, dx);
+		yForceAdd(b, dy);
+	}
 
-        private double xForceAdd(Node a, double x) {
-                if (xForces.contains(a)) {
-                        x += xForces.get(a);
-                }
-                xForces.put(a, x);
-                return x;
-        }
+	private double xForceAdd(Node a, double x) {
+		if (xForces.contains(a)) {
+			x += xForces.get(a);
+		}
+		xForces.put(a, x);
+		return x;
+	}
 
-        private double yForceAdd(Node a, double y) {
-                if (yForces.contains(a)) {
-                        y += yForces.get(a);
-                }
-                yForces.put(a, y);
-                return y;
-        }
+	private double yForceAdd(Node a, double y) {
+		if (yForces.contains(a)) {
+			y += yForces.get(a);
+		}
+		yForces.put(a, y);
+		return y;
+	}
 
-        public void triggerChange(Graph g, Change change) {
-                if (g == lastLayout) {
-                        graphChanged = true;
-                } else {
-                        lastLayout = null;
-                        graphChanged = true;
-                }
-        }
+	public void triggerChange(Graph g, Change change) {
+		if (g == lastLayout) {
+			graphChanged = true;
+		} else {
+			lastLayout = null;
+			graphChanged = true;
+		}
+	}
 
 }
